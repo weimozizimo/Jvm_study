38b945bae06b082fbae69b908c9682d7963b3973
wyf <zimo@example.com>
2021-02-10 10:42:09 +0800

    增加3.6.3代码

---
+++ src/main/java/chapter3/section6/Lesson3.java
@@ -0,0 +1,12 @@
+package chapter3.section6;
+
+/**
+*@description 长期存活的对象将进入老年代
+*@author weiyifei
+*@date 2021/2/10
+*/
+public class Lesson3 {
+    public static void main(String[] args) {
+
+    }
+}
